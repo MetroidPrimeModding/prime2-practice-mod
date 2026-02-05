927a9842c89e7f79f562ffd39492228e707e26bc
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-02-05 20:22:54 +0200

    Fix paths in build_and_patch_iso.sh

--- build_and_patch_iso.sh
+++ build_and_patch_iso.sh
@@ -3,9 +3,9 @@
 # call build with all params; this will set some env vars we use later
 source ./build.sh "$@"
 
-docker run --rm -v "${SCRIPT_DIR}/..":"${DOCKER_SRC_DIR}" "${IMAGE}" bash -c "\
+docker run --rm -v "${EXTERNAL_SRC_DIR}":"${DOCKER_SRC_DIR}" "${IMAGE}" bash -c "\
   gcn-static-patcher-cli \
-    -m \"${DOCKER_SRC_DIR}/cmake-build-release-docker/prime2-practice\" \
-    -i \"${DOCKER_SRC_DIR}/prime2.iso\" \
-    -o \"${DOCKER_SRC_DIR}/prime2-practice-mod.iso\" \
+    -m \"${DOCKER_BUILD_DIR}/prime2-practice\" \
+    -i \"${DOCKER_SRC_DIR}prime2.iso\" \
+    -o \"${DOCKER_SRC_DIR}prime2-practice-mod.iso\" \
     --overwrite"
