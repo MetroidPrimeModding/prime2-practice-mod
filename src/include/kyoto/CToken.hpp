737e04f3a58025eeca9c0703037094034d51dc92
duncathan <dunc@duncathan.com>
2026-05-14 10:10:17 -0600

    fix crash on loading new worlds, and hopefully the memory leaks?

--- src/include/kyoto/CToken.hpp
+++ src/include/kyoto/CToken.hpp
@@ -5,11 +5,15 @@
 
 class CToken {
 public:
-  CToken() {}
-  CToken(CObjectReference* ref);
+  CToken() : x0_objRef(nullptr), x4_lockHeld(false) {}
+  CToken(CObjectReference* ref) : x0_objRef(ref), x4_lockHeld(false) {
+    x0_objRef->AddReference();
+  }
   CToken(IObj* obj); // : x0_objRef(new CObjectReference(obj)), x4_lockHeld(false) {}
   CToken(const CToken& other);
-  ~CToken();
+  // ~CToken();
+
+  CToken* CToken_dtor(u16 param);
 
   CObjOwnerDerivedFromIObjUntyped* GetObj();
   void Lock();
@@ -24,6 +28,16 @@ public:
   bool HasLock() { return x4_lockHeld; }
 
   const CObjectReference* GetRef() const { return x0_objRef; }
+  inline CObjectReference* GetRefNonConst() { return x0_objRef; }
+
+  // for crimes
+  inline void InitializeFromOther(const CToken& other) {
+    x0_objRef = other.x0_objRef;
+    x0_objRef->AddReference();
+    if (other.x4_lockHeld) {
+      Lock();
+    }
+  }
 
 private:
   CObjectReference* x0_objRef;
